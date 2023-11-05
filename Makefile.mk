884e62ac4861f443752425d2cd637f490886a47e
yong park <yong.park123456@gmail.com>
2023-11-05 00:04:16 -0600

    changed the names

--- Makefile.mk
+++ Makefile.mk
@@ -1,7 +1,7 @@
 all: pgm.o	hough
 
-hough:	houghBase.cu pgm.o
-	nvcc houghBase.cu pgm.o -o hough -arch=sm_75
+hough:	hough.cu pgm.o
+	nvcc hough.cu pgm.o -o hough -arch=sm_86
 
 pgm.o:	common/pgm.cpp
 	g++ -c common/pgm.cpp -o ./pgm.o
